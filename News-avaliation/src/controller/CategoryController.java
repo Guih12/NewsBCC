ab4a4716f450d9448f49ba4024951e7229f00768
George Borsato <georginhoborsato@gmail.com>
2020-12-22 17:11:19 -0300

    att

--- News-avaliation/src/controller/CategoryController.java
+++ News-avaliation/src/controller/CategoryController.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package controller;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 import model.Category;
 
 /**
@@ -12,4 +16,20 @@ import model.Category;
  */
 public class CategoryController extends GenericController<Category>{
     
+    public List<Category> searchCategory(String description){
+        EntityManager em = getEntityManager();
+        
+        try{
+            
+             TypedQuery<Category> query = em.createQuery("Select c from Category c where c.description LIKE :description", Category.class); 
+             query.setParameter("description", "%"+description+"%");
+             List<Category> categories  = query.getResultList();
+             return categories;
+            
+        }catch(NoResultException e){
+            return null;
+        }finally{
+            em.close();
+        }
+    }
 }
--- News-avaliation/src/controller/NewsController.java
+++ News-avaliation/src/controller/NewsController.java
@@ -1,8 +1,27 @@
 
 package controller;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 import model.News;
 
 public class NewsController extends GenericController<News>{
-    
+ 
+  
+    public News getNewsByPk(int id){
+        EntityManager em = getEntityManager();
+        
+        try{
+            TypedQuery<News> query = em.createQuery("Select n from News n where n.id = :id", News.class);
+            query.setParameter("id", id);
+            News news = query.getSingleResult();
+            return news;
+            
+        }catch(NoResultException ex){
+            return null;
+        }finally{
+            em.close();
+        }
+    }
 }
